245e122b0c5d88061df6c8d1297905f534d210e8
Greg <ginman86@gmail.com>
2018-10-05 15:02:55 -0600

    Add SimpleFormWithGrid

--- packages/ra-ui-materialui/src/form/index.js
+++ packages/ra-ui-materialui/src/form/index.js
@@ -1,6 +1,7 @@
 export FormInput from './FormInput';
 export FormTab from './FormTab';
 export SimpleForm from './SimpleForm';
+export SimpleFormWithGrid from './SimpleFormWithGrid';
 export SimpleFormIterator from './SimpleFormIterator';
 export TabbedForm from './TabbedForm';
 export Toolbar from './Toolbar';
